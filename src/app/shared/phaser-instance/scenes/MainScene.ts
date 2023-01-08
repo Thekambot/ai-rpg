84c3a3bbdff27d04355ed59a35ce4d0b0964e3f7
Thekambot <thekambot@gmail.com>
2023-01-09 00:14:11 +0100

    Phaser Fixes

--- src/app/shared/phaser-instance/scenes/MainScene.ts
+++ src/app/shared/phaser-instance/scenes/MainScene.ts
@@ -4,12 +4,21 @@ export class MainScene extends Phaser.Scene {
   constructor() {
     super({ key: 'main' });
   }
-
-  create() {
-    console.log('create method');
-  }
+  private shape!: Phaser.GameObjects.Image;
 
   preload() {
-    console.log('preload method');
+    console.log('Main Scene Preload Start');
+
+    this.load.image('shape', 'assets/shapes.svg');
+
+    console.log('Main Scene Preload End');
+  }
+
+  create() {
+    console.log('Main Scene Create Start');
+
+    // this.shape = this.add.image(400, 300, 'shape');
+
+    console.log('Main Scene Create End');
   }
 }
